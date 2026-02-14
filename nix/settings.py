bae5992d64373b8fb517d9f658ecfba7a258138b
Mostafa Raihan <raihan.invite@gmail.com>
2026-02-14 21:31:44 +0600

    add

--- nix/settings.py
+++ nix/settings.py
@@ -17,12 +17,12 @@ DEBUG = True
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
-    'babyandfashion.com',
+    'babyandfashion.vercel.app',
     'localhost',
     '127.0.0.1'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://babyandfashion.com']
+CSRF_TRUSTED_ORIGINS = ['https://babyandfashion.vercel.app']
 
 AUTH_USER_MODEL = 'main.User'
 
